95a5da1963dde38bcf5df4af0c775e162c4edba9
giri_chandu@yahoo.co.in <giri_chandu@yahoo.co.in>
2021-05-10 08:29:58 +0800

    Post input section

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+
+app.post('/',(req,res)=>
+{
+var email=req.body.email;
+var amount=req.body.amount;
+
+res.send({"amount":amount,"email":email});
+
+});
+
+app.listen(3000,()=>{
+    console.log ('server is running');
+});
